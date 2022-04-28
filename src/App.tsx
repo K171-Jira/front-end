b12b91a4e43cbd7d7f991c39d225fc0fffca53e7
ADaunys <augustas.daunys@ktu.edu>
2022-04-28 15:59:56 +0300

    Map View Created

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,15 @@ import { Route, Routes } from 'react-router-dom';
 import MaskList from './masks/components/MaskList';
 import MaskEdit from './masks/components/MaskEdit';
 import QRPage from './common/components/QRPage';
+import MapComponent from './map/MapComponent';
+import { Wrapper, Status } from '@googlemaps/react-wrapper';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const render = (status: Status) => {
+    return <h1>{status}</h1>;
+  };
   return (
     <QueryClientProvider client={queryClient}>
       <NavBar />
@@ -19,6 +24,14 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<SignupPage />} />
         <Route path="/qr/:id" element={<QRPage />} />
+        <Route
+          path="/map"
+          element={
+            <Wrapper apiKey={'AIzaSyDtztCf2KWCjaqn1B20KNCfZfsHpyWyZgQ'} render={render}>
+              <MapComponent />
+            </Wrapper>
+          }
+        />
         <Route path="/masks">
           <Route path="add" element={<MaskEdit />} />
           <Route path=":id" element={<MaskEdit />} />
---
+++ src/map/MapComponent.tsx
@@ -0,0 +1,38 @@
+import { Box } from '@mantine/core';
+import React from 'react';
+import { GoogleMap } from 'react-google-maps';
+import withGoogleMap from 'react-google-maps/lib/withGoogleMap';
+import withScriptjs from 'react-google-maps/lib/withScriptjs';
+
+function Map() {
+  return <GoogleMap defaultZoom={12} defaultCenter={{ lat: 54.8985, lng: 23.9036 }} />;
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function MapComponent() {
+  return (
+    <Box style={{ width: '80vw', height: '70vh', margin: '0 auto' }}>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDtztCf2KWCjaqn1B20KNCfZfsHpyWyZgQ`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </Box>
+  );
+}
+
+// const MapComponent: React.FC<{}> = () => {
+//   const ref = React.useRef<HTMLDivElement>(null);
+//   const [map, setMap] = React.useState<google.maps.Map>();
+
+//   React.useEffect(() => {
+//     if (ref.current && !map) {
+//       setMap(new window.google.maps.Map(ref.current, {}));
+//     }
+//   }, [ref, map]);
+//   return <div ref={ref} />;
+// };
+
+// export default MapComponent;
