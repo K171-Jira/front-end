9a57b2c767194edee7f8e6cd7749b911354bf6d4
Aurvas <79143711+Aurvas@users.noreply.github.com>
2022-06-21 15:48:52 +0300

    Update PaymentForm.tsx

--- src/payment/PaymentForm.tsx
+++ src/payment/PaymentForm.tsx
@@ -21,21 +21,31 @@ import {
   Loader,
   Center,
   Container,
-  PasswordInput,
+  NumberInput,
 } from '@mantine/core';
 
 import User from '../users/models/User';
 import { AuthContext } from '../authentication/context/AuthContext';
 import { UserContextType } from '../authentication/models/User';
+import { useMutation } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+import { getValue } from '@testing-library/user-event/dist/utils';
 
 export default function PaymentForm() {
+  const navigate = useNavigate();
+  const [amount, setValue] = useState(5);
   const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [text, setText] = useState('');
   const { user, addBalance } = useContext(AuthContext) as UserContextType;
   const stripe = useStripe();
   const elements = useElements();
-
+  
   const handleSubmit = async (e: any) => {
     e.preventDefault();
+    setLoading(true);
+	  var selectedAmount = amount;
+    Math.trunc(selectedAmount);
     if (stripe === null) {
       throw new Error(`BUG: stripe is null`);
     }
@@ -50,20 +60,21 @@ export default function PaymentForm() {
       type: 'card',
       card: element,
     });
-
+    
     if (!error) {
       try {
         const { id } = paymentMethod;
         const _id = user?._id;
         const balance = user?.balance;
         const response = await axios.post(`${API_URL}/payments`, {
-          id,
+		       id,
           _id,
           balance,
+		      selectedAmount,
         });
         if (response.data.success) {
           console.log('Apmokėjimas pavyko');
-          addBalance(10);
+          addBalance(amount);
           setSuccess(true);
         } else {
           console.log('Error', response.data);
@@ -73,15 +84,30 @@ export default function PaymentForm() {
       }
     } else {
       console.log(error.message);
+      setText(String(error.message));
     }
+    setLoading(false)
   };
 
   return (
     <Container style={{ marginTop: '100px' }}>
       <Card shadow="xl" p="md" sx={{ maxWidth: 400 }} mx="auto" withBorder>
+      
         {!success ? (
+          
           <form className="form">
             <Title order={1}>Apmokėjimas</Title>
+            <NumberInput
+            style={{ marginTop: 15 }}
+            label="Pasirinkite taškų kiekį"
+            description="Vieno taško vertė - 1 euras"
+            stepHoldDelay={500}
+            stepHoldInterval={100}
+            max={25}
+            min={5}
+            value= {amount}
+            onChange={(value) => setValue(Number(value))}
+            />
             <Space h="md" />
             <fieldset className="FormGroup">
               <div className="FormRow">
@@ -89,6 +115,9 @@ export default function PaymentForm() {
               </div>
             </fieldset>
             <Space h="md" />
+            <Text color="red">{text}  </Text>
+            {loading? (<Loader color="lime" size="md" variant="bars" />):
+            (
             <Button
               variant="outline"
               color="lime"
@@ -98,11 +127,12 @@ export default function PaymentForm() {
               onClick={handleSubmit}
             >
               Mokėti
-            </Button>
+            </Button>)}
           </form>
         ) : (
           <div>
             <Title order={1}>Sumokėta</Title>
+           
           </div>
         )}
       </Card>
